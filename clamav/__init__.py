a65e04881b36de9c6b35245daa15aef15d0c915c
PsypherPunk <psypherpunk@gmail.com>
2023-03-11 14:00:24 +0000

    fix!: move app to clamav module
    - move `app` to its proper place in the `clamav` module.
    - BREAKING CHANGE: remove `/v1` prefix.
    - remove some Commitizen config. to permit normal version-bump
      behaviour.

---
+++ clamav/__init__.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+
+from clamav.api import instream, ping, version
+
+app = FastAPI(
+    title="ClamAV API",
+    description="Implementation of a HTTP API over ClamAV.",
+    contact={
+        "email": "psypherpunk+github@gmail.com",
+    },
+)
+
+app.include_router(instream.router)
+app.include_router(ping.router)
+app.include_router(version.router)
